6bb207eeb77f0c5f96b865c2ab90c4d4b32691f2
hedgaralves <121463776+hedgaralves@users.noreply.github.com>
2023-06-20 16:24:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using Projetos_.NET.Models;
+
+
+Calculadora calc = new Calculadora();
+
+// calc.Somar(10, 30);
+// calc.Subtrair(10,50);
+// calc.Multiplicar(15,45);
+// calc.Dividir(2,2);
+// calc.Potencia(3, 3);
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.WriteLine("Digite uma letra");
+// string letra = Console.ReadLine();
+
+// switch(letra)
+// {   
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//         Console.WriteLine("Vogal");
+//         break;
+
+//     default:
+//         Console.WriteLine("Nao e uma vogal");
+//         break;
+// }
+
+
+
+// int quantidadeEmEstoque = 3;
+// int quantidadeCompra = 0;
+// bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+
+
+// Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+// Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+// Console.WriteLine($"E possivel realizar venda? {possivelVenda}");
+
+// if (quantidadeCompra == 0)
+// {
+//     Console.WriteLine("Venda invalida");
+// }
+// else if(possivelVenda)
+// {
+//     Console.WriteLine("Venda realizada.");
+// }
+// else
+// {
+//     Console.WriteLine("Desculpe. Nao temos a quantidade desejada em estoque.");
+// }
+
+
+
+
+
+
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Hedgar";
+// pessoa1.Idade = 34;
+// pessoa1.Apresentar();
